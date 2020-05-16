b4c91210e99be37eccf705a134539b48248cd30d
Fei Yang <klxfeiyang@gmail.com>
2020-05-16 12:49:01 -0700

    Rename config to config_path for clarity
    Authored-by: Fei Yang <klxfeiyang@gmail.com>

--- autoclop.rb
+++ autoclop.rb
@@ -2,21 +2,21 @@ require 'shellwords'
 require 'yaml'
 
 def run_autoclop                   # TODO: several methods with similar names
-  config = ENV['AUTOCLOP_CONFIG'] # TODO: global variable config
+  config_path = ENV['AUTOCLOP_CONFIG'] # TODO: global variable config
   os = File.read('/etc/issue')    # TODO: global variable $config and ENV
   user = ENV['USER']
-  autoclop(os, config, user)
+  autoclop(os, config_path, user)
 end
 
-def autoclop(os, config, user)   # TODO: multiple responsibilities; configuration and invocation
+def autoclop(os, config_path, user)   # TODO: multiple responsibilities; configuration and invocation
   cmd =
-    if config.nil? || config.empty?  # TODO: nil check; TODO: order dependencies; TODO: anonymous boolean logic
+    if config_path.nil? || config_path.empty?  # TODO: nil check; TODO: order dependencies; TODO: anonymous boolean logic
       Kernel.puts 'WARNING: No file specified in $AUTOCLOP_CONFIG. Assuming the default configuration.'
       clop_cmd(py_version(os, {}), 'O2', "-L/home/#{esc user}/.cbiscuit/lib")
     else
-      cfg = YAML.safe_load(File.read(config))  # TODO: coupling to both format (yaml) and data source
+      cfg = YAML.safe_load(File.read(config_path))  # TODO: coupling to both format (yaml) and data source
       if cfg.nil? # TODO: nil check
-        Kernel.puts "WARNING: Invalid YAML in #{config}. Assuming the default configuration."
+        Kernel.puts "WARNING: Invalid YAML in #{config_path}. Assuming the default configuration."
         clop_cmd(py_version(os, {}), 'O2', "-L/home/#{esc user}/.cbiscuit/lib")
       else
         libargs =
