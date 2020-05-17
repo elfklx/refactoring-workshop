0c5224c6df2aca100cb1a22fffb1ba12cf2d20c4
Fei Yang <klxfeiyang@gmail.com>
2020-05-17 14:56:01 -0700

    Avoid reading the file twice
    Authored-by: Fei Yang <klxfeiyang@gmail.com>

--- autoclop.rb
+++ autoclop.rb
@@ -35,11 +35,11 @@ class ConfigFactory
     if path.nil? || path.empty? # TODO: nil check; TODO: order dependencies; TODO: anonymous boolean logic
       ['WARNING: No file specified in $AUTOCLOP_CONFIG. Assuming the default configuration.',
        DefaultConfig.new(user)]
-    elsif from_yaml(path, user).invalid?
+    elsif (c = from_yaml(path, user)).invalid?
       ["WARNING: Invalid YAML in #{path}. Assuming the default configuration.",
       DefaultConfig.new(user)]
     else
-      ['', from_yaml(path, user)]
+      ['', c]
     end
   end
 
